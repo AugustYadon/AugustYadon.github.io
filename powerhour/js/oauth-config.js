90107fb2eb7a874be35c80aefd00690c6c3d5b76
August Yadon <AugustYadon@gmail.com>
2018-10-27 13:05:35 -0400

    trying something

--- powerhour/js/oauth-config.js
+++ powerhour/js/oauth-config.js
@@ -6,9 +6,9 @@ var OAuthConfig = (function() {
   "My Applications" section of the Spotify developer site */
   var clientId = '4a8875631f9145bc846b13c7c4d150e5';
   var redirectUri = 'http://localhost:8000/callback.html';
-
-  if (location.href.indexOf('http://www.auggodoggogames.com') === 0) {
-    redirectUri = 'http://www.auggodoggogames.com/powerhour/callback.html';
+  console.log("This is what to look for: " + location.href);
+  if (location.href.indexOf('http://auggodoggogames.com') === 0) {
+    redirectUri = 'http://auggodoggogames.com/powerhour/callback.html';
   }
 
   var host = /http[s]?:\/\/[^/]+/.exec(redirectUri)[0];

024321a377a85aa4f1b8456740a91a629e78f4b4
August Yadon <AugustYadon@gmail.com>
2018-10-27 15:12:50 -0400

    back

--- powerhour/js/oauth-config.js
+++ powerhour/js/oauth-config.js
@@ -5,10 +5,10 @@ var OAuthConfig = (function() {
   /* replace these values with yours obtained in the
   "My Applications" section of the Spotify developer site */
   var clientId = '4a8875631f9145bc846b13c7c4d150e5';
-  var redirectUri = 'http://localhost:8000/callback.html';
+  var redirectUri = 'http://localhost:8000/powerhour/callback.html';
 
-  if (location.href.indexOf('http://auggodoggogames.com') === 0) {
-    redirectUri = 'auggodoggogames.com/powerhour/callback.html';
+  if (location.href.indexOf('http://www.auggodoggogames.com') === 0) {
+    redirectUri = 'http://auggodoggogames.com/powerhour/callback.html';
   }
 
   var host = /http[s]?:\/\/[^/]+/.exec(redirectUri)[0];

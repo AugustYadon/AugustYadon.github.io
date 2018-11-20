1cc66f3dfc6956e27eb8e6fb3abc5b5a6044c764
August Yadon <AugustYadon@gmail.com>
2018-11-19 21:27:45 -0500

    Maybe not async...

--- powerhour/js/oauth-config.js
+++ powerhour/js/oauth-config.js
@@ -7,24 +7,29 @@ var OAuthConfig = (function() {
   var clientId = '4a8875631f9145bc846b13c7c4d150e5';
   var redirectUri = 'http://localhost:8000/powerhour/callback.html';
 
-  if (location.href.indexOf('http://auggodoggogames.com') === 0 || location.href.indexOf('auggodoggogames.com') === 0 || location.href.indexOf('http://www.auggodoggogames.com') === 0) {
+  if (location.href.indexOf('http://auggodoggogames.com/powerhour/') === 0){
+    redirectUri = 'http://auggodoggogames.com/powerhour/callback.html';
+    console.log("YES");  
+    console.log(location.href);  
+  }  else if(location.href.indexOf('http://www.auggodoggogames.com/powerhour/') === 0) {
     redirectUri = 'http://www.auggodoggogames.com/powerhour/callback.html';
     console.log("YES");  
     console.log(location.href);  
+  }  else{ 
+    console.log("NOPE");  
+    console.log(location.href);  
   }
-  else{
-    console.log("NOPE");  console.log(location.href);  }
 
-  setTimeout(function(){ 
-    var host = /http[s]?:\/\/[^/]+/.exec(redirectUri)[0];
+  //setTimeout(function(){ 
+  var host = /http[s]?:\/\/[^/]+/.exec(redirectUri)[0];
 
-    return {
-      clientId: clientId,
-      redirectUri: redirectUri,
-      host: host,
-      stateKey: 'spotify_auth_state'
-    };
-        }, 1000);
+  return {
+    clientId: clientId,
+    redirectUri: redirectUri,
+    host: host,
+    stateKey: 'spotify_auth_state'
+  };
+ // }, 3000);
 
 
 })();
